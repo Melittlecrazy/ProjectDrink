99f34184138cb228202ea6ca27610c1e7fab86c0
Marc Gutierrez <zerreitugcram200@gmail.com>
2019-11-14 17:09:09 -0600

    added obstical function

---
+++ ProjectDrink/Assets/Scripts/Obstical.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstical : MonoBehaviour
+{
+
+    public GameObject loseText;
+    private AudioSource audioSource;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //Debug.Log("touched trigger");
+        if (collision.tag == "Player")
+        {
+            audioSource.Play();
+            loseText.SetActive(true);
+        }
+    }
+}
